5976ff9cad59e6b72c655648dde2920817233af0
bchuminx <bensabenteuer@gmail.com>
2022-11-01 16:19:24 +0800

    Add further changes to the search query processing

--- Dev/healthhub_app.py
+++ Dev/healthhub_app.py
@@ -20,41 +20,46 @@ st.set_page_config(
 )
 
 def get_similar(entity):
-    with driver.session() as session:
-        query = """
-                match (n)-[:related_to]-(x)
-                where (n.type = 'Disease' or n.type = 'Condition') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
-                return distinct x.name as Most_Similar
-                """
-    return session.run(query)
+    if entity != '':
+        with driver.session() as session:
+            query = """
+                    match (n)-[:related_to]-(x)
+                    where (n.type = 'Disease' or n.type = 'Condition') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
+                    return distinct x.name as Most_Similar
+                    """
+        return session.run(query)
 
 def get_definition(entity):
-    with driver.session() as session:
-        query = """
-                match (n)
-                where (n.type = 'Disease' or n.type = 'Condition') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
-                return distinct n.text as Definition, n.source as Source
-                """
-    return session.run(query)
+    if entity != '':
+        with driver.session() as session:
+            query = """
+                    match (n)
+                    where (n.type = 'Disease' or n.type = 'Condition') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
+                    return distinct n.text as Definition, n.source as Source
+                    """
+        return session.run(query)
 
 def get_answer(entity, type):
     type = type.title()
-    with driver.session() as session:
-        query = """
-                match (n)-[r]-(x)
-                where n.type =~ '(?i)"""+type+"""' and r.name =~ '(?i)""" + entity + """' and not x.name =~ '(?i)""" + entity + """'
-                return distinct x.type as Class,  x.name as """+type+""", r.source as Source, r.text as Notes
-                """
-    return session.run(query)
+    if entity != '' and type != '':
+        with driver.session() as session:
+            query = """
+                    match (n)-[r]-(x)
+                    where n.type =~ '(?i)"""+type+"""' and r.name =~ '(?i)""" + entity + """' and not x.name =~ '(?i)""" + entity + """'
+                    return distinct x.type as Class,  x.name as """+type+""", r.source as Source, r.text as Notes
+                    """
+            #print(query)
+        return session.run(query)
 
 def get_info(entity):
-    with driver.session() as session:
-        query = """
-                match (n:Info)-[r]-(x)
-                where r.name =~ '(?i)""" + entity + """'
-                return distinct r.text as Info, r.source as Source
-                """
-    return session.run(query)
+    if entity != '':
+        with driver.session() as session:
+            query = """
+                    match (n:Info)-[r]-(x)
+                    where r.name =~ '(?i)""" + entity + """'
+                    return distinct r.text as Info, r.source as Source
+                    """
+        return session.run(query)
     
 st.title('Health Knowledge Hub')
 
@@ -83,13 +88,15 @@ if query != '':
     
     for token in doc:
         #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
-        if token.dep_ in ['nsubj', 'conj', 'ROOT', 'dobj'] and token.pos_ == 'NOUN':
+        if token.dep_ in ['nsubj', 'conj', 'ROOT', 'dobj', 'pobj'] and token.pos_ == 'NOUN':
             if token.lemma_ in ['medication', 'medicine', 'treatment']:
                 search_type = 'prescription'
             elif token.lemma_ == 'management':
                 search_type = 'management'
             elif token.lemma_ == 'screening':
                 search_type = 'checkup'
+            elif token.lemma_ in ['risk']:
+                search_type = 'riskfactor'
             elif token.lemma_ in ['bill', 'payment', 'subsidy']:
                 search_type = 'expenses'
             else:
@@ -101,8 +108,6 @@ if query != '':
                 search_type='management'
             elif token.lemma_ in ['cure', 'medicate', 'treat']:
                 search_type = 'prescription'
-            elif token.lemma_ in ['cause', 'risk']:
-                search_type = 'riskfactor'
             elif token.lemma_ in ['check', 'detect', 'test']:
                 search_type = 'test'
             elif token.lemma_ in ['screen']:
@@ -121,17 +126,22 @@ if query != '':
 
         if search_types:
             for search_type in search_types:
-                print("XXX:",search_type)
+                #print("Search Type:",search_type)
                 answer = get_answer(ent.text, search_type)
                 if answer is not None:
                     data = json.dumps([r.data() for r in answer])
                     results_df = pd.read_json(data)
 
                     if not results_df.empty:
+                        if 'Riskfactor' in results_df.columns:
+                            results_df = results_df.rename({'Riskfactor':'Risk Factor'},axis=1)
+
+                        answer_header = str(results_df.columns[1]).title()
+
                         with col2:
                             st.markdown(''.join(['''<p style='color:#FDFD96;
                                         font-size:18px;
-                                        text-align:left'>''',"",search_type.title(),"</style></p><"]),unsafe_allow_html=True)
+                                        text-align:left'>''',"",answer_header,"</style></p><"]),unsafe_allow_html=True)
                         for name, group in results_df.groupby('Source'):
                             with col2:
                                 st.markdown(''.join(['''<i><p style='color:RoyalBlue;
@@ -154,8 +164,6 @@ if query != '':
                                         group.Source = group.Source.fillna("-")
                                     if 'Notes' in group.columns:
                                         group["Notes"] = group["Notes"].fillna("-")
-                                    if 'Riskfactor' in group.columns:
-                                        group = group.rename({'Riskfactor':'Risk Factor'},axis=1)
                                     group = group.drop('Source', axis=1)
                                     #group = group.rename(columns=group.iloc[0]).drop(group.index[0])
                                     st.table(group)
