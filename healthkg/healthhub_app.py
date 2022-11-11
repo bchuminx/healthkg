6777c8e2088c37ae3b8868c475ec52f9ffd63ba3
joeytxy <joeytxy0706@gmail.com>
2022-11-11 18:29:07 +0800

    Add filter option for medication and display for recommended definition

--- healthkg/healthhub_app.py
+++ healthkg/healthhub_app.py
@@ -31,7 +31,7 @@ def get_similar(entity):
         with driver.session() as session:
             query = """
                     match (n)-[:related_to]-(x)
-                    where (n.type = 'Disease' or n.type = 'Condition') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
+                    where (n.type = 'Disease' or n.type = 'Condition' or n.type = 'Medication') and n.name =~ '(?i)""" + entity + """' and n.text <> ""
                     return distinct x.name as Most_Similar, n.name as Name
                     """
         return session.run(query)
@@ -45,6 +45,7 @@ def get_definition(entity):
                     return distinct n.text as Definition, n.source as Source, n.name as Name
                     """
         return session.run(query)
+
 
 def get_primary_answer(entity, type):
     type = type.title()
@@ -64,7 +65,7 @@ def get_secondary_answer(entity, subject, object):
         query = """
                 match (x)-[r1]-(n)-[r2]-(y)
                 where n.type =~ '(?i)"""+subject+"""' and n.name =~ '(?i)""" + entity + """' and x.type =~ '(?i)"""+object+"""' and not y.type =~ '(?i)"""+object+"""'
-                return distinct r2.name as Type, y.name as """+subject+"""
+                return distinct r2.name as Type, y.name as """+subject+""",r2.source as Source,r2.text as Notes
                 """
     return session.run(query)
 
@@ -107,18 +108,18 @@ if query != '':
     doc = nlp(query)
     
     for token in doc:
-        #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
-
         if token.dep_ in ['nsubj'] and token.pos_ == 'NOUN':
             if token.lemma_ in ['effect']:
                 subject = 'effect'
-        
+            elif token.lemma_ in ['instruction']:
+                subject='instruction'
+            elif token.lemma_ in ['precaution']:
+                subject='precaution'
         elif token.dep_ in ['dobj', 'pobj', 'nmod'] and token.pos_ == 'NOUN':
             if token.lemma_ in ['vaccine']:
                 object = 'vaccination'
             elif token.lemma_ in ['medicine', 'medication']:
                 object = 'medication'
-
         if token.dep_ in ['nsubj', 'nmod', 'conj', 'ROOT', 'dobj', 'pobj'] and token.pos_ == 'NOUN':
             if token.lemma_ in ['effect']:
                 search_type = 'effect'
@@ -158,22 +159,94 @@ if query != '':
         most_similar = get_similar(ent.text)
         definition = get_definition(ent.text)
         info = get_info(ent.text)
-
         if subject != "" and object != "":
             secondary_answer = get_secondary_answer(ent.text, subject, object)
-
+            ans_dict={}
             if secondary_answer is not None:
                 data = json.dumps([r.data() for r in secondary_answer])
                 results_df = pd.read_json(data)
-
                 if 'Type' in results_df.columns:
                     for name, group in results_df.groupby('Type'):
-                        with col2:
-                            st.markdown(''.join(['''<p style='color:#daa520;
-                                        font-size:18px;
-                                        text-align:left'>''',"",""+name,"</style></p><"]),unsafe_allow_html=True)
-                            group = group.drop('Type', axis=1)
-                            st.table(group)
+                        if get_similar(name) is not None:
+                            med_class_data = json.dumps([r.data() for r in get_similar(name)])
+                            med_class_df = pd.read_json(med_class_data)
+                            if 'Most_Similar' in med_class_df.columns:
+                                for index,row in med_class_df.iterrows():
+                                    grp_item = row['Most_Similar']
+                                    if grp_item not in ans_dict:
+                                        ans_dict[grp_item]=[[name,group]]
+                                    else:
+                                        ans_dict[grp_item].append([name,group])
+                            else:
+                                ans_dict[name]=[[name,group]]
+                                                
+                with col2:
+                    if ans_dict!={}:
+                        answer_selection=st.radio("Select an option/group to view its corresponding answers", ans_dict.keys())
+                        if len(ans_dict[answer_selection])==1:
+                            for i in ans_dict[answer_selection]:
+                                st.markdown(''.join(['''<p style='color:#daa520;
+                                                        font-size:18px;
+                                                        text-align:left'>''',"",""+i[0],"</style></p><"]),unsafe_allow_html=True)
+                                if 'Type' in i[1].columns:
+                                    i[1]=i[1].drop('Type',axis=1)
+                                if 'Source' in i[1].columns:
+                                    for source_name,med_ans in i[1].groupby('Source'):
+                                        st.markdown(''.join(['''<i><p style='color:RoyalBlue;
+                                                            font-size:15px;
+                                                            text-align:right'>''',"Source: ",source_name,"</style></p></i>"]),unsafe_allow_html=True)
+                                        if not med_ans.empty:
+                                            med_ans=med_ans.drop('Source',axis=1)
+                                            if 'Effect' in med_ans.columns:
+                                                med_ans = med_ans.rename({'Effect':'Side Effect'},axis=1)
+                                                med_ans = med_ans[~med_ans['Side Effect'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                                            elif 'Instruction' in med_ans.columns:
+                                                med_ans = med_ans[~med_ans['Instruction'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                                            elif 'Precaution' in med_ans.columns:
+                                                med_ans = med_ans[~med_ans['Precaution'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]                                            
+                                            if med_ans['Notes'].isna().sum()==med_ans.shape[0]:
+                                                med_ans = med_ans.drop('Notes', axis=1)
+                                            if 'Notes' in med_ans.columns:
+                                                med_ans['Notes'] = med_ans['Notes'].fillna("-")
+                                            st.table(med_ans)
+                        else:
+                            list_of_options=[]
+                            specific_options=[]
+                            col2.caption("Select an option belonging to this group to view its corresponding answers. You may select more than one to compare.")
+                            for i in ans_dict[answer_selection]:
+                                list_of_options.append(i[0])
+                                specific_med_selection = col2.checkbox(i[0])
+                                if specific_med_selection:
+                                    specific_options.append(i[0])
+                            for i in specific_options:
+                                st.markdown(''.join(['''<p style='color:#daa520;
+                                                            font-size:18px;
+                                                            text-align:left'>''',"",""+i,"</style></p><"]),unsafe_allow_html=True)
+                                ans=ans_dict[answer_selection][list_of_options.index(i)][1]
+                                if 'Type' in ans.columns:
+                                    ans=ans.drop('Type',axis=1)
+                                if 'Source' in ans.columns:
+                                    for source_name,med_ans in ans.groupby('Source'):
+                                        st.markdown(''.join(['''<i><p style='color:RoyalBlue;
+                                                            font-size:15px;
+                                                            text-align:right'>''',"Source: ",source_name,"</style></p></i>"]),unsafe_allow_html=True)
+                                        if not med_ans.empty:
+                                            med_ans=med_ans.drop('Source',axis=1)
+                                            if 'Effect' in med_ans.columns:
+                                                med_ans = med_ans.rename({'Effect':'Side Effect'},axis=1)
+                                                med_ans = med_ans[~med_ans['Side Effect'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                                            elif 'Instruction' in med_ans.columns:
+                                                med_ans = med_ans[~med_ans['Instruction'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                                            elif 'Precaution' in med_ans.columns:
+                                                med_ans = med_ans[~med_ans['Precaution'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                                            if med_ans['Notes'].isna().sum()==med_ans.shape[0]:
+                                                med_ans = med_ans.drop('Notes', axis=1)
+                                            if 'Notes' in med_ans.columns:
+                                                med_ans['Notes'] = med_ans['Notes'].fillna("-")
+                                            st.table(med_ans)                                   
+
+                            
+
 
         if search_types:
             for search_type in search_types:
@@ -183,7 +256,6 @@ if query != '':
                 if primary_answer is not None:
                     data = json.dumps([r.data() for r in primary_answer])
                     results_df = pd.read_json(data)
-
                     if not results_df.empty:
                         name_label = results_df['Name'][0]
 
@@ -191,8 +263,11 @@ if query != '':
                             results_df = results_df.rename({'Riskfactor':'Risk Factor'},axis=1)
                         elif 'Effect' in results_df.columns:
                             results_df = results_df.rename({'Effect':'Side Effect'},axis=1)
-                            results_df = results_df[~results_df['Side Effect'].str.contains("COVID-19")]
-
+                            results_df = results_df[~results_df['Side Effect'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                        elif 'Instruction' in results_df.columns:
+                            results_df = results_df[~results_df['Instruction'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
+                        elif 'Precaution' in results_df.columns:
+                            results_df = results_df[~results_df['Precaution'].str.contains("COVID-19|Diabetes|High Blood Pressure|High Cholesterol|Stroke|Colorectal Cancer")]
                         answer_header = str(results_df.columns[0]).title()
 
                         with col2:
@@ -209,7 +284,7 @@ if query != '':
                             with col2:
                                 st.markdown(''.join(['''<i><p style='color:RoyalBlue;
                                             font-size:15px;
-                                            text-align:left'>''',"Source: ",name,"</style></p></i>"]),unsafe_allow_html=True)
+                                            text-align:right'>''',"Source: ",name,"</style></p></i>"]),unsafe_allow_html=True)
 
                             if group['Notes'].isna().sum()==group.shape[0]:
                                     group = group.drop('Notes', axis=1)
@@ -227,7 +302,6 @@ if query != '':
                                     group = group.drop(['Name', 'Source'], axis=1)
                                     #group = group.rename(columns=group.iloc[0]).drop(group.index[0])
                                     st.table(group)
-
 
         if definition is not None:
             data = json.dumps([r.data() for r in definition])
@@ -257,35 +331,59 @@ if query != '':
                 if similar_item is not "":
                     similar_item_header = "Related to " + results_df['Name'][0] + ": "
                     st.info(similar_item_header + similar_item)
-
             with col1:
-                related_lst=[]
+                related_lst1=[] 
                 related_info_lst=[]
+                related_lst2=[] 
+                related_def_lst=[]
                 for index, row in results_df.iterrows():
-                        item = row['Most_Similar']
-                        recommended_info = get_info(item)
-                        if recommended_info is not None:
-                            data = json.dumps([r.data() for r in recommended_info])
-                            results_df = pd.read_json(data)
-                            if 'Info' in results_df.columns:
-                                related_info_header = "Info for " + item
-                                results_df = results_df.rename({'Info':related_info_header}, axis=1)
-                                related_lst.append(item)
+                    item = row['Most_Similar']
+                    recommended_info = get_info(item)
+                    recommended_def = get_definition(item)
+                    if recommended_info is not None:
+                        data = json.dumps([r.data() for r in recommended_info])
+                        results_df = pd.read_json(data)
+                        if 'Info' in results_df.columns:
+                            related_info_header = "Info for " + item
+                            results_df = results_df.rename({'Info':related_info_header}, axis=1)
+                            related_lst1.append(item)
 
-                                for name, group in results_df.groupby('Source'):
-                                    group = group.drop(['Name', 'Source', 'Type'], axis=1)
-                                    related_info_lst.append([name, group])
+                            for name, group in results_df.groupby('Source'):
+                                group = group.drop(['Name', 'Source', 'Type'], axis=1)
+                                related_info_lst.append([name, group])
                                 
-                if related_lst!=[]:
+                    if recommended_def is not None:
+                        data = json.dumps([r.data() for r in recommended_def])
+                        results_df = pd.read_json(data)
+                        if 'Definition' in results_df.columns:
+                            related_def_header = "Definition for " + item
+                            results_df = results_df.rename({'Definition':related_def_header}, axis=1)
+                            related_lst2.append(item)
+                            for name, group in results_df.groupby('Source'):
+                                group = group.drop(['Name', 'Source'], axis=1)
+                                related_def_lst.append([name, group])
+                if related_lst2!=[]:
+                    st.markdown(''.join(['''<p style='color:#daa520;
+                                        font-size:18px;
+                                        text-align:left'> Recommended Definition </style></p>''']), unsafe_allow_html=True)
+
+                    selection=st.radio("Select disease/condition to see more definitions",related_lst2)
+                    st.markdown(''.join(['''<i><p style='color:RoyalBlue;
+                                                font-size:15px;
+                                                text-align:right'>''',"Source: ", related_def_lst[related_lst2.index(selection)][0],"</style></p></i>"]), unsafe_allow_html=True)
+                    st.table(related_def_lst[related_lst2.index(selection)][1])
+                    
+                if related_lst1!=[]:
                     st.markdown(''.join(['''<p style='color:#daa520;
                                         font-size:18px;
                                         text-align:left'> Recommended Info </style></p>''']), unsafe_allow_html=True)
 
-                    selection=st.radio("Select disease/condition to see more info",related_lst)
+                    selection=st.radio("Select disease/condition to see more info",related_lst1)
                     st.markdown(''.join(['''<i><p style='color:RoyalBlue;
                                                 font-size:15px;
-                                                text-align:right'>''',"Source: ", related_info_lst[related_lst.index(selection)][0],"</style></p></i>"]), unsafe_allow_html=True)
-                    st.table(related_info_lst[related_lst.index(selection)][1])
+                                                text-align:right'>''',"Source: ", related_info_lst[related_lst1.index(selection)][0],"</style></p></i>"]), unsafe_allow_html=True)
+                    st.table(related_info_lst[related_lst1.index(selection)][1])
+
 
         if info is not None:
             data = json.dumps([r.data() for r in info])
